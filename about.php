493d21b1b2ebbeb37210feba66e43a23f792d8aa
Conor Gilmer <conor.gilmer@gmail.com>
2016-12-12 23:57:03 +0000

    comments

--- about.php
+++ about.php
@@ -8,7 +8,7 @@
       <div class="row">
         <div class="col-lg-9">
           <h2>About</h2>
-          <p>Webpages looking at downloading Stock data from Yahoo Finance! (NYSE, NASDAQ and AMEX) and Quandl(WIKI and FRED). Currency converter, and Currency rates and historical datai relative to the euro  are also available from Yahoo and Quandl.</p>
+          <p>Webpages looking at downloading Stock data from Yahoo Finance! (NYSE, NASDAQ and AMEX) and Quandl(WIKI and FRED). Currency converter, and Currency rates and historical datai relative to the euro  are also available from Yahoo Finance!  and Quandl.</p>
 
         <p>Boostrap frontend and Google Charts used on data gleaned from these online services and sometimes stored locally on MySQL DB..</p>
         <p> Highbeam is aslo used to display some charts, however I am using only its free 50 views per day version.</p>
